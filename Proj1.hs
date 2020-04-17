061ab68b067f0a83e7f1ae1da82269cd95b8159a
Will_Zhu <will_zyq@qq.com>
2020-04-17 15:57:24 +1000

    set Location framework

--- Proj1.hs
+++ Proj1.hs
@@ -8,23 +8,41 @@
 module Proj1 (Location, toLocation, feedback,
               GameState, initialGuess, nextGuess) where
 
-data Location = Location Col Col
-                deriving (Eq, Show)
+import Data.Char
 
-data Col = A|B|C|D|E|F|G|H
-                deriving (Eq, Show)
+type Col = Int
+type Row = Int
+data Location = Location (Col, Row) deriving (Eq, Read)
+
+instance Show Location where
+    show (Location (col, row))
+        = [toUpper (intToDigit (col + 9)), intToDigit row]
+
+toCol :: Char -> Int
+toCol x = (digitToInt x) - 9
+
+inBoard :: String -> Bool
+inBoard [col_char, row_char]
+    = (elem col_char ['A'..'H']) && (elem row_char ['1'..'4'])
+inBoard _ = False
 
 data GameState = GG
                 deriving (Eq, Show)
 
 toLocation :: String -> Maybe Location
-toLocation _ = Just (Location A A)
+toLocation xs =
+    if inBoard xs
+        then Just (Location (col, row))
+        else Nothing
+    where
+        col = toCol (head xs)
+        row = digitToInt (last xs)
 
 feedback :: [Location] -> [Location] -> (Int, Int, Int)
 feedback _ _ = (1,1,1)
 
 initialGuess :: ([Location], GameState)
-initialGuess = ([(Location A A)], GG)
+initialGuess = ([(Location (1,1))], GG)
 
 nextGuess :: ([Location], GameState) -> (Int, Int, Int) -> ([Location], GameState)
-nextGuess _ _ = ([(Location A A)], GG)
+nextGuess _ _ = ([(Location (1,1))], GG)
