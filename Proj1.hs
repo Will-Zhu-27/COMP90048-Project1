ec8f828c5f7721191087884f567cdfcbad41edc1
Will_Zhu <will_zyq@qq.com>
2020-04-20 10:29:46 +1000

    filter only by previous guesses and feedback

--- Proj1.hs
+++ Proj1.hs
@@ -7,12 +7,12 @@
 
 module Proj1 (Location, toLocation, feedback,
               GameState, initialGuess, nextGuess) where
-
 import Data.Char
+import Data.List
 
 type Col = Int
 type Row = Int
-data Location = Location (Col, Row) deriving (Eq, Read)
+data Location = Location (Col, Row) deriving (Eq, Ord)
 
 instance Show Location where
     show (Location (col, row)) = [chr (64 + col), intToDigit row]
@@ -63,18 +63,66 @@ feedback targets (x:xs) =
 data GameState = GameState [(Location, Location, Location)] deriving (Show, Eq)
 
 initialGuess :: ([Location], GameState)
-initialGuess = ([(Location (1,1))], all_combos)
-    where
-        all_combos = GameState [((Location (x1, y1)),(Location (x2, y2)),(Location (x3, y3)))
-            | x1 <- [1..8]
-            , y1 <- [1..4]
-            , x2 <- [1..8]
-            , y2 <- [1..4]
-            , (x1 * 10 + y1) < (x2 * 10 + y2)
-            , x3 <- [1..8]
-            , y3 <- [1..4]
-            , (x2 * 10 + y2) < (x3 * 10 + y3)
-            ]
+initialGuess = ([(Location (1,1))], allCombos)
+
+allCombos :: GameState
+allCombos = GameState [((Location (x1, y1)),(Location (x2, y2)),(Location (x3, y3)))
+    | x1 <- [1..8]
+    , y1 <- [1..4]
+    , x2 <- [1..8]
+    , y2 <- [1..4]
+    , (x1 * 10 + y1) < (x2 * 10 + y2)
+    , x3 <- [1..8]
+    , y3 <- [1..4]
+    , (x2 * 10 + y2) < (x3 * 10 + y3)
+    ]
+
+
+allLocations :: [Location]
+allLocations = [Location (x, y) | x <- [1..8], y <- [1..4]]
 
 nextGuess :: ([Location], GameState) -> (Int, Int, Int) -> ([Location], GameState)
-nextGuess _ _ = ([(Location (1,1))], GameState [])
+nextGuess (guesses, (GameState combos)) result = ([l1, l2, l3], (GameState filter_combos))
+    where
+        GameState filter_combos = filterByCombos (GameState combos) guesses result
+        (l1, l2, l3) = head filter_combos
+
+start :: [Location] -> Int
+start targets =
+    let
+        (initial_guesses, initial_gamestate) = initialGuess
+    in repeatGuesses targets initial_guesses initial_gamestate
+
+repeatGuesses :: [Location] -> [Location] -> GameState-> Int
+repeatGuesses targets guesses gamestate
+    |(sort targets) == (sort guesses) = 1
+    |otherwise = 
+        let
+            (next_guesses, next_gamestate) = nextGuess (guesses, gamestate) (feedback targets guesses)
+        in 1 + (repeatGuesses targets next_guesses next_gamestate)
+
+-- 将剩下的组合用上一个guesses进行筛选
+filterByCombos :: GameState -> [Location] -> (Int, Int, Int) -> GameState
+filterByCombos (GameState lst) pre_guesses result =
+    GameState (filter (\(l1, l2, l3) -> feedback [l1,l2,l3] pre_guesses == result) lst)
+
+-- 用于根据猜的位置对GameState进行筛选
+filterByLocation :: GameState -> [Location] -> Int -> GameState
+filterByLocation gamestate [] _ = gamestate
+filterByLocation (GameState combos_lst) loc_lst n = (GameState filter_lst)
+    where filter_lst = filter (\x -> sameLocationNum x loc_lst == n) combos_lst
+
+sameLocationNum :: (Location, Location, Location) -> [Location] -> Int
+sameLocationNum (loc1, loc2, loc3) [] = 0
+sameLocationNum (loc1, loc2, loc3) loc_lst =
+    length (filter (`elem` loc_lst) [loc1, loc2, loc3])
+
+-- 筛选出给定距离的给定交叉的位置，是否要去除掉猜的位置？？？
+distanceFilter :: [Location] -> Int -> Int -> [Location]
+distanceFilter loc_lst dis overlap_num = [Location (x, y) 
+    | Location (x, y) <- allLocations
+    , (length (filter (\loc -> (distance (Location (x,y)) loc) == Just dis) loc_lst)) == overlap_num]
+
+distanceLocation :: Location -> Int -> [Location]
+distanceLocation (Location (col, row)) dis = 
+    [Location (col + x, row + y) | x <- [(-dis)..dis], (x + col) > 0 && (x + col) < 9, y <- [(-dis)..dis], (y + row) > 0 && (y + row) < 5, (max (abs x) (abs y)) == dis]
